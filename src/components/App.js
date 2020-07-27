5f497f546753ef31b1b9b4481f3362f39b229234
narencoderpad <narencoderpad@gmail.com>
2020-07-27 14:39:09 +0530

    wepack changes

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component{
+    render(){
+        return(
+            <div>
+                <h1>My first bolier plate app in React with Webpack</h1>                
+            </div>
+        );
+    }
+}
+export default App;
